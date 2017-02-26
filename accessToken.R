88737fef0b911167f47785bc3b725431729ae959
Cedric Ith <cedric.ith1@gmail.com>
2017-02-26 15:27:28 -0800

    removed repeat access.token var

--- accessToken.R
+++ accessToken.R
@@ -6,6 +6,5 @@ search.query <- list(grant_type = "client_credentials", client_id = "oUBxMAlVRNv
 response <- POST("https://api.yelp.com/oauth2/token", query = search.query)
 response
 body <- fromJSON(content(response, "text"))
-access_token <- body$access_token 
-access.token <- access_token
+access_token <- body$access_token
 
