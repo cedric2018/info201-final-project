71198191e8542128e4a7ac073163e25099793d22
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-02-26 15:26:16 -0800

    Add updated file with access.code

--- accessToken.R
+++ accessToken.R
@@ -7,5 +7,8 @@ response <- POST("https://api.yelp.com/oauth2/token", query = search.query)
 response
 body <- fromJSON(content(response, "text"))
 access_token <- body$access_token 
-access.token <- access_token
-
+#USE access.code for headers
+access.code <- paste0("bearer ", access_token)
+#Example of GET call with authorization
+# response.first <- GET("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972", add_headers(Authorization = access.code))
+# body.first <- fromJSON(content(response.first, "text"))    
