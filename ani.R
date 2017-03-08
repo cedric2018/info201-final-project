9779cd22b5cd85f2ceb05122593cc22655bdc400
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-03-07 18:20:15 -0800

    Add my file

--- ani.R
+++ ani.R
@@ -3,26 +3,34 @@ library(leaflet)
 library(shiny)
 library(httr)
 library(jsonlite)
+library(maps)
+library(ggplot2)
+#install.packages("geojsonio")
+library(geojsonio)
+library("zipcode")
 
 source("accessToken.R")
-
+#source("~/Desktop/UW/Quarter 2/INFO 201/Assignments/a6-data-visualization-ask710/Practice2.R")
 access.code
-
-response <-GET ("https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972", add_headers(Authorization = access.code))
-body <- fromJSON(content(response, "text"))    
+input <- "Seattle"
+query <- list(location = input)
+response <-GET ("https://api.yelp.com/v3/businesses/search?term=food&limit=50", query = query, add_headers(Authorization = access.code))
+response
+body <- fromJSON(content(response, "text"))
 data <- body$businesses
+
+query.second <- list(offset = 50)
+response.second <- GET("https://api.yelp.com/v3/businesses/search?term=food&location=98105&limit=50", query = query.second, add_headers(Authorization = access.code))
+body.second <- fromJSON(content(response.second, "text"))    
+data.second <- body.second$businesses
+
 coordinates <- data$coordinates
 coordinates$longitude
-
+categories <- data$categories
+View(categories)
 View(body$businesses)
 typeof(data)
 
-m <- leaflet() %>%
-   addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng= coordinates$longitude, lat= coordinates$latitude, popup= paste(data$name, "<br>",
-                                                                                 "Price:", data$price,"<br>",
-                                                                                 "Rating:", data$rating))
- m
- 
+
 
 
